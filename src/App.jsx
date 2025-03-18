478ebd0f8d0ae3af6b93d4e19176b485f9ca21a2
danielcaicedoj <daniel.caicedoj@gmail.com>
2025-03-18 14:45:05 -0500

    Add build files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Services from './components/Services';
 import './App.css';
 
 function App() {
