0fef39a76422cd0c06351187899c3be7507e9d00
danielcaicedoj <daniel.caicedoj@gmail.com>
2025-03-18 15:45:33 -0500

    Configurar Vite y Github actions para despliegue en github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/moiicol-website/',
+  build: {
+    outDir: 'dist', // Asegúrate de que la carpeta de salida sea 'dist'
+  },
+});
